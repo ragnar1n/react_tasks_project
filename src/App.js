d83ba10232f32a99fd3624b4eb167342f95dfef1
Ragnar <ragnar.naruski@voco.ee>
2023-03-07 21:52:53 +0200

    Add Login

--- src/App.js
+++ src/App.js
@@ -1,37 +1,64 @@
-import React, {useState} from "react";
+import React, {useEffect,useState} from "react";
 import './App.css';
+import Login from "./components/Login/Login";
+import AuthContext from "./components/Login/auth-context";
 
+import Home from "./components/Login/Home";
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
+import MainHeader from "./components/Login/MainHeader/MainHeader";
 
 const tasksD=[
     {
         id: 'e1',
         date: new Date(2023,0,10),
-        title:'study c++',
-        priority:'low'
+        title:'Study JS',
+        priority:'medium'
     },
     {
         id:'e2',
-        date: new Date(2024,0,5),
-        title:'study js',
+        date: new Date(2023,1,5),
+        title:'Study React',
         priority:'high'
     }
 ]
 
 const App = ()=> {
 
+    const [isLoggedIn,setIsLoggedIn]=useState(false)
     const [tasks,setTasks]=useState(tasksD)
     const addTaskhandler=(task)=>{
         setTasks((previousTasks)=>{
             return [task,...previousTasks]
         })
     }
+    useEffect(()=>{
+        const storedUserLoggedIn=localStorage.getItem('isLoggedIn')
+        if (storedUserLoggedIn ==='1'){
+            setIsLoggedIn(true)
+        }
+    },[])
+
+    const loginHandler = (email, password) => {
+        localStorage.setItem('isLoggedIn','1')
+        setIsLoggedIn(true);
+    };
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn')
+        setIsLoggedIn(false);
+    };
   return (
-    <div className="App">
-        <NewTask onAddTask={addTaskhandler}></NewTask>
+      <AuthContext.Provider value={{isLoggedIn: isLoggedIn,
+          onLogout: logoutHandler}}>
+          <MainHeader onLogout={logoutHandler}/>
+    <main className="App">
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {isLoggedIn && <NewTask onAddTask={addTaskhandler}></NewTask>}
         <Tasks tasks={tasks}></Tasks>
-    </div>
+    </main>
+      </AuthContext.Provider>
   );
 }
 
